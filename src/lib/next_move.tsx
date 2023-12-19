49f4d424d2cf8db1f44252ce18db7284b9d9d33d
taroj1205 <taroj1205@gmail.com>
2023-12-19 13:33:05 +1300

    upgrade the ai

--- src/lib/next_move.tsx
+++ src/lib/next_move.tsx
@@ -12,10 +12,38 @@ function findBestMove(grid: (string | null)[][]): number | null {
     return winningMove;
   }
 
+  // If no blocking or winning move is found, place randomly
+  let availableColumns = [];
   for (let col = 0; col < grid[0].length; col++) {
     if (grid[0][col] === null) {
-      return col;
+      availableColumns.push(col);
     }
+  }
+
+  // Check if placing a piece would lead the opponent to win in the next move
+  let safeColumns = [];
+  for (let col of availableColumns) {
+    for (let row = grid.length - 1; row >= 0; row--) {
+      if (grid[row][col] === null) {
+        grid[row][col] = ourPlayer; // Simulate our move
+        if (!findWinningMove(grid, opponent)) { // If the opponent cannot win in the next move
+          safeColumns.push(col);
+        }
+        grid[row][col] = null; // Undo our move
+        break;
+      }
+    }
+  }
+
+  if (safeColumns.length > 0) {
+    let randomIndex = Math.floor(Math.random() * safeColumns.length);
+    return safeColumns[randomIndex];
+  }
+
+  // If no safe column is found, place randomly in the available columns
+  if (availableColumns.length > 0) {
+    let randomIndex = Math.floor(Math.random() * availableColumns.length);
+    return availableColumns[randomIndex];
   }
 
   return null;
